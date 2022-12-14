5f899d6672155030f9b96e593f0cd4d63d2003fa
Vladimir Cuneaz <vladimirsette.vc@gmail.com>
2022-12-14 21:53:54 +0100

    Fix mongodb url

--- src/configs/mongoose.configuration.ts
+++ src/configs/mongoose.configuration.ts
@@ -4,8 +4,7 @@ import { getEnvVar } from 'src/helpers/getEnvVar.helper';
 export const configureMongooseModule = () => {
   return MongooseModule.forRootAsync({
     useFactory: () => ({
-      uri: `mongodb://${getEnvVar('mongo_host')}:27017,
-      )}/discord`,
+      uri: `mongodb://${getEnvVar('mongo_host')}:27017/discord`,
       authSource: 'discord',
       user: getEnvVar('mongo_user'),
       pass: getEnvVar('mongo_pass'),
