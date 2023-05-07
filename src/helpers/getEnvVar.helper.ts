ef126d5eab3ecc93ea3284d0e0b4e75cf98b0206
Deepacks <vladimirsette.vc@gmail.com>
2023-05-07 15:33:12 +0200

    Update docker secret prefix

--- src/helpers/getEnvVar.helper.ts
+++ src/helpers/getEnvVar.helper.ts
@@ -3,4 +3,4 @@ import { secrets } from 'docker-secret'
 export const getEnvVar: (n: string) => string = (n) =>
   process.env.NODE_ENV === 'development'
     ? process.env[n]
-    : secrets[`DISCORD_BOT_${n}`]
+    : secrets[`DISCORD_${n}`]
